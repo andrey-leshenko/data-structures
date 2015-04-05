f0cbfac81370c4496f300ff97bd4e738bbd21b28
Andrey Leshenko <andrey.leshenko@gmail.com>
2015-04-05 17:06:43 +0300

    Added a binary heap with some examples

---
+++ examples/BinaryHeapExamples.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace DataStructures.Examples
+{
+    class BinaryHeapExamples
+    {
+        public static void Example1()
+        {
+            BinaryHeap b = new BinaryHeap();
+            b.Add(5);
+            b.Add(7);
+            b.Add(3);
+            b.Add(0);
+            b.Add(15);
+            b.Add(8);
+
+            while (b.Count() > 0) {
+                Console.Write(b.PopMax() + ", ");
+            }
+
+            // Output:
+            // 15, 8, 7, 5, 3, 0, 
+        }
+
+        public static void Example2()
+        {
+            int[] unsorted = { 5, 9, 3, 4, };
+            BinaryHeap b = new BinaryHeap(unsorted);
+
+            int[] downSorted = new int[unsorted.Length];
+
+            for (int i = 0; i < downSorted.Length; i++) {
+                downSorted[i] = b.PopMax();
+            }
+
+            // downSorted is [9, 5, 4, 3]
+        }
+    }
+}
---
+++ src/BinaryHeap.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections.Generic;
+
+namespace DataStructures
+{
+    class BinaryHeap
+    {
+        const int root = 0;
+
+        List<int> keys;
+
+        public BinaryHeap()
+        {
+            keys = new List<int>();
+        }
+
+        public BinaryHeap(int[] keysCollection)
+        {
+            this.keys = new List<int>(keysCollection);
+            for (int i = keys.Count / 2; i >= 0; i--)
+                OrderDownFrom(i);
+        }
+
+        public int Max()
+        {
+            return keys[0];
+        }
+
+        public int PopMax()
+        {
+            int max = Max();
+            MoveLastToRoot();
+            OrderDownFrom(root);
+
+            return max;
+        }
+
+        public void Add(int key)
+        {
+            keys.Add(key);
+            OrderUpFrom(keys.Count - 1);
+        }
+
+        public int Count()
+        {
+            return keys.Count;
+        }
+
+        void OrderDownFrom(int node)
+        {
+            while (!IsLeaf(node) && keys[MaxChild(node)] > keys[node]) {
+                SwapWithParent(MaxChild(node));
+                node = MaxChild(node);
+            }
+        }
+
+        void OrderUpFrom(int node)
+        {
+            while (!IsRoot(node) && keys[node] > keys[Parent(node)]) {
+                SwapWithParent(node);
+                node = Parent(node);
+            }
+        }
+
+        void SwapWithParent(int node)
+        {
+            int temp = keys[node];
+            keys[node] = keys[Parent(node)];
+            keys[Parent(node)] = temp;
+        }
+
+        int MaxChild(int node)
+        {
+            if (!HasRightChild(node))
+                return LeftChild(node);
+
+            int left = LeftChild(node);
+            int right = RightChild(node);
+
+            return keys[right] > keys[left] ? right : left;
+        }
+
+        void MoveLastToRoot()
+        {
+            keys[0] = keys[keys.Count - 1];
+            keys.RemoveAt(keys.Count - 1);
+        }
+
+        int Parent(int node)            { return (node - 1) / 2; }
+
+        int LeftChild(int node)         { return 2 * node + 1; }
+
+        int RightChild(int node)        { return 2 * node + 2; }
+
+        bool HasLeftChild(int node)     { return LeftChild(node) < keys.Count; }
+
+        bool HasRightChild(int node)    { return RightChild(node) < keys.Count; }
+
+        bool IsRoot(int node)           { return node == 0; }
+
+        bool IsLeaf(int node)           { return !HasLeftChild(node); }
+    }
+}
